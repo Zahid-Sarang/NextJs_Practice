34ee60dff0472c5b80e2ca635af35885cdc76b08
zahid-soft <zahidsarang95@gmail.com>
2023-09-11 22:32:54 +0530

    signup form working and user store in data base

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,22 +1,24 @@
-import mongoose from "mongoose";
-import { connected } from "process";
+import mongoose from 'mongoose';
 
 export async function connect() {
-	try {
-		mongoose.connect(process.env.MONGO_URL!);
-		const connection = mongoose.connection;
+    try {
+        mongoose.connect(process.env.MONGO_URL!);
+        const connection = mongoose.connection;
 
-		connection.on("connected", () => {
-			console.log("Database connection established");
-		});
-		connection.on("error", (err) => {
-			console.log(
-				"Database connection error. please make sure your database connection." +
-					err
-			);
-			process.exit();
-		});
-	} catch (err) {
-		console.log(err);
-	}
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
+        })
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
+        })
+
+    } catch (error) {
+        console.log('Something goes wrong!');
+        console.log(error);
+        
+    }
+
+
 }
